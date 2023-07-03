4a5d1f11f2d8ce586d92cec8b44239d4a49645be
Abbas Aziz <abbas9111@gmail.com>
2023-07-03 17:44:45 +0500

    Added Room creation

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-
-
+import RoomJoinPage from './RoomJoinPage';
+import CreateRoomPage from './CreateRoomPage';
+import { BrowserRouter as Router, Routes, Route, Link, Redirect,} from "react-router-dom";
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
     }
     render(){
-        return <h1>Test Website made with React and Django</h1>
+        return (
+        <Router>
+            <Routes>
+                <Route exact path='/'element= <p>This is the home page of the app</p>/>
+                <Route path='/join' element={<RoomJoinPage/>}/>
+                <Route path='/create' element={<CreateRoomPage/>}/>
+            </Routes>
+        </Router>)
     }
 }
